e516613238ac13c1bb2a7af74d7c6a29a51e81b8
newbieflank <raninovia188@gmail.com>
2024-09-05 15:02:13 +0700

    ADD:    - htaccess         - config         - public index

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../config/config.php';
+require_once '../app/core/App.php';
+require_once '../app/core/Controller.php';
+require_once '../app/core/Model.php';
+require_once '../app/core/Router.php';
+require_once '../app/core/View.php';
+
+$app = new App();
