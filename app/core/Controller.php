42cd30efb2926cb24a435e8972fed88db315d686
newbieflank <raninovia188@gmail.com>
2024-09-12 09:08:44 +0700

    UPDATE: - register

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,8 +2,16 @@
 
 class Controller
 {
+    private $baseURL;
+
+    public function __construct()
+    {
+        $this->baseURL = "http://localhost/web_rekost/public/";
+    }
     public function view($view, $data = [])
     {
+        $data['baseURL'] = $this->baseURL;
+
         extract($data);
         require_once '../app/views/' . $view . '.php';
     }
---
+++ app/views/login/register.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="<?= BASEURL; ?>css/registerPage.css">
+<style>
+    @media (max-width: 1024px) {
+        .loginPage img {
+            display: none;
+        }
+
+        .login-content img {
+            display: block;
+            position: absolute;
+            top: -20;
+            right: -20;
+            width: 100px;
+            height: 80px;
+        }
+    }
+</style>
+<div class="loginPage">
+    <img src="<?= BASEURL; ?>img/ImageLogin.svg" alt="Image">
+
+
+    <div class="login-content">
+        <img class="loginImage" src="<?= BASEURL; ?>img/ImageLoginLogo.svg" alt="">
+        <h2>Get Started!</h2>
+        <p>Already have an account? <a href="<?= BASEURL; ?>register">Sign In?</a></p>
+
+
+        <form action="">
+            <label for="fullname">Full Name</label>
+            <input type="text" name="fullname" id="fullname" placeholder="Enter full name" required>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Enter email" required>
+            <label for="number">Phone Number</label>
+            <input type="tel" name="number" id="number" placeholder="Enter phone number" required>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter password" required>
+            <button type="submit">Create account</button>
+        </form>
+    </div>
+</div>
