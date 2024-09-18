f706e477fe5cfd09aed47d2b2c0e904fd0d0de42
newbieflank <raninovia188@gmail.com>
2024-09-18 21:18:41 +0700

    UPDATE: -several Update

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ Router::get('/', 'HomeController@index');
 Router::get('/login', 'LoginController@login');
 Router::get('/register', 'LoginController@register');
 Router::get('/id/{id}', 'LoginController@show');
+Router::get('/profile', 'ProfileController@profile');
+// Router::get('/profile/{id}', 'ProfileController@profile');
