2727f875059f3d431f76e9d6d8d2c6fd03bc084a
newbieflank <raninovia188@gmail.com>
2024-09-22 16:30:25 +0700

    UPDATE: -Update Routeing in landing, login and register

--- app/views/home/landingpage.php
+++ app/views/home/landingpage.php
@@ -44,10 +44,10 @@
                 </ul>
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a href="login.php" class="btn btn-outline-primary mr-2">Sign In</a>
+                        <a href="login" class="btn btn-outline-primary mr-2">Sign In</a>
                     </li>
                     <li class="nav-item">
-                        <a href="register.php" class="btn btn-primary">Sign Up</a>
+                        <a href="register" class="btn btn-primary">Sign Up</a>
                     </li>
                 </ul>
             </div>
--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?= BASEURL; ?>css/loginPage.css">
+<link rel="stylesheet" href="css/loginPage.css">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
 <style>
     @media (max-width: 1024px) {
@@ -26,7 +26,7 @@
 </style>
 <div class="Page">
     <div class="content">
-        <img class="logo" src="<?= BASEURL; ?>img/ImageLoginLogo.svg" alt="">
+        <img class="logo" src="img/ImageLoginLogo.svg" alt="">
         <h2>Selamat Datang</h2>
         <p>Selamat Datang di laman Re-kost! Tempat terbaik untuk mencari rekomendasi kost</p>
 
@@ -35,7 +35,7 @@
             <input type="text" name="username" id="username" placeholder="example@gmail.com" required>
             <div class="password-section">
                 <label for="password">Password</label>
-                <img class="showPass" src="<?= BASEURL; ?>img/eyePass.svg" alt="" onclick="togglePassword()">
+                <img class="showPass" src="img/eyePass.svg" alt="" onclick="togglePassword()">
             </div>
             <input type="password" name="password" id="password" placeholder="Masukan Password" required>
             <div class="remember-section">
@@ -49,13 +49,13 @@
             <span>Or Sign Up With</span>
         </div>
         <a href="" class="button-link">
-            <img src="<?= BASEURL; ?>img/googleIcon.svg" alt="Logo" class="button-logo">
+            <img src="img/googleIcon.svg" alt="Logo" class="button-logo">
             Sign In With Google
         </a>
     </div>
 
     <div class="img-wrap">
-        <img src="<?= BASEURL; ?>img/ImageLogin.svg" alt="">
+        <img src="img/ImageLogin.svg" alt="">
     </div>
 </div>
 
