0335feb682dc8b593cfadce22e43bdd8a9a18a47
newbieflank <raninovia188@gmail.com>
2024-10-03 14:26:22 +0700

    UPDATE

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,14 +4,14 @@ class Controller
 {
 
 
-    public function view($view, $data = [])
+    protected function view($view, $data = [])
     {
 
         extract($data);
         require_once '../app/views/' . $view . '.php';
     }
 
-    public function model($model)
+    protected function model($model)
     {
         require_once '../app/models/' . $model . '.php';
         return new $model;
