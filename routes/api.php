c74781fd4fcced77f6c5c55d1f506f3684e37c74
newbieflank <raninovia188@gmail.com>
2024-11-25 08:06:10 +0700

    update in api register

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,5 @@ Router::post('/api/login', 'UserController@login');
 Router::get('/api/imgProfile/{id}', 'UserController@getProfileImage');
 Router::get('/api/kos', 'KosController@getKos');
 Router::post('/api/upload', 'FileController@upload');
+
+Router::post('/api/register', 'UserController@register');
