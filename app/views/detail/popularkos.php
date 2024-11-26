1def46a1d74d6c0bb86f059ed5154af3ce4d6347
newbieflank <raninovia188@gmail.com>
2024-11-26 22:37:46 +0700

    update

--- app/views/detail/popularkos.php
+++ app/views/detail/popularkos.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <link rel="stylesheet" href="style.css">
     <style>
         html {
             scroll-behavior: smooth;
@@ -25,6 +24,7 @@
             display: flex;
             flex-direction: column;
             border: 1px solid #ddd;
+            border-radius: 20px;
         }
 
         .card-body {
@@ -87,11 +87,11 @@
                             <i class="fas fa-map-marker-alt"></i> Lokasi
                         </button>
                         <div class="dropdown-menu" aria-labelledby="dropdownLokasi">
-                            <a class="dropdown-item" href="#">Blindungan</a>
-                            <a class="dropdown-item" href="#">Tamanan</a>
-                            <a class="dropdown-item" href="#">Tamansari</a>
-                            <a class="dropdown-item" href="#">Tapen</a>
-                            <a class="dropdown-item" href="#">Sempol</a>
+                            <a class="dropdown-item" href="Blindungan">Blindungan</a>
+                            <a class="dropdown-item" href="Tamanan">Tamanan</a>
+                            <a class="dropdown-item" href="Tamansari">Tamansari</a>
+                            <a class="dropdown-item" href="Tapen">Tapen</a>
+                            <a class="dropdown-item" href="Sempol">Sempol</a>
                         </div>
                     </div>
                     <div class="dropdown d-inline-block mr-3">
@@ -126,26 +126,28 @@
         <div class="container-fluid px-4">
             <div class="row" style="margin-top: 32px;">
                 <?php foreach ($data['popular'] as $popular): ?>
-                    <div class="col-md-3 p-3 mb-5">
+                    <div class="col-md-3 mb-4">
                         <a href="<?= BASEURL . 'detailkos/' . $popular["id_kos"] ?>" class="card-link">
                             <div class="card">
-                                <img src="<?= asset('img/home1.png') ?>" class="card-img-top" height="200" width="300" />
+                                <img src="<?= asset('uploads/' . $popular["id_kos"] . '/foto_depan.jpg') ?>"
+                                    class="card-img-top" alt="Kost Image">
                                 <div class="card-body">
-                                    <h5 class="card-title"><?php echo $popular['nama_kos'] ?></h5>
-                                    <span class="btn-available"
-                                    style="border-radius: 4px;"><?php echo $popular['tipe_kos'] ?></span>
+                                    <h5 class="card-title" style="font-size: 20px; font-weight: bold;">
+                                        <?php echo $popular['nama_kos'] ?>
+                                    </h5>
+                                    <span class="btn-available mb-3" style="border-radius: 4px;">
+                                        <?php echo $popular['tipe_kos'] ?></span>
                                     <p class="card-text mt-3" style="font-size: 14px;"><i class="fas fa-map-marker-alt"></i>
-                                        <?php echo $popular['alamat'] ?></p>
+                                        <?php echo $popular['alamat'] ?>
+                                    </p>
+
                                     <p class="card-text" style="font-weight: 600;"><?php echo $popular['avg_rating'] ?>/5
                                         (<?php echo $popular['review_count'] ?>)</p>
-                                    <span class="btn-available"
-                                        style="border-radius: 4px;"><?php echo $popular['status_kamar'] ?></span>
                                     <p class="card-text" style="font-size: 20px; font-weight: bold; color: #E52424;">
                                         IDR <?php echo $popular['harga'] ?>
                                         <span
                                             style="font-size: 16px; font-weight: normal; color:#4A4A4A">/<?php echo $popular['waktu_penyewaan'] ?></span>
                                     </p>
-                                    <a class="btn-order" href="detailkos">Pesan sekarang</a>
                                 </div>
                             </div>
                         </a>
