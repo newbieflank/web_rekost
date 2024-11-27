6beeb17377f73ce2b2327b1c31db57e9d415ea8a
newbieflank <raninovia188@gmail.com>
2024-11-27 18:25:41 +0700

    update

--- config/config.php
+++ config/config.php
@@ -1,9 +1,4 @@
 <?php
-define('BASEURL',  'http://192.168.1.237/web_rekost/');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'rekost');
 define('BASEURL',  $_ENV['BASEURL']);
 
 define('DB_HOST', $_ENV['DB_HOST']);
